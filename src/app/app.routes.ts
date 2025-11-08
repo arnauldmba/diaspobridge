d8b7440894b787530bc494a4a97d9f0c0f4c14bb
Arnauld-Mba <arnauldmba@gmail.com>
2025-11-08 21:47:01 +0100

    feat: add create-annonce functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MyListings } from './my-listings/my-listings';
 import { Listings } from './listing/listings';
+import { AddListing } from './add-listing/add-listing';
 
 export const routes: Routes = [
     { path: "my-listings", component: MyListings },
     { path: "listings", component: Listings },
+    { path: "add-listing", component: AddListing },
 ];
