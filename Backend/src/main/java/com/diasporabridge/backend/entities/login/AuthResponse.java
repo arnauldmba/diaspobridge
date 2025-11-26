7efd86b79283645de1fb0d7a322bb61bfe89870f
Arnauld-Mba <arnauldmba@gmail.com>
2025-11-26 16:37:12 +0100

    chore: ignore Eclipse metadata folder

--- Backend/src/main/java/com/diasporabridge/backend/entities/login/AuthResponse.java
+++
@@ -1,3 +0,0 @@
-package com.diasporabridge.backend.entities.login;
-
-public record AuthResponse(String token, Long userId, String role) {}
--- Backend/src/main/java/com/diasporabridge/backend/entities/login/LoginDto.java
+++
@@ -1,6 +0,0 @@
-package com.diasporabridge.backend.entities.login;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginDto(@Email @NotBlank String email, @NotBlank String password) {}
