6d59196ab3e857574fec0d0d5cb0aa2a7582cff2
Arnauld-Mba <arnauldmba@gmail.com>
2026-02-04 18:44:22 +0100

    chore: reorganize project structure into frontend and backend

---
+++ frontend/.angular/cache/20.3.8/diasporabridge/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,51 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-7QCEAMJY.js";
+import "./chunk-5EG33CFQ.js";
+import {
+  Dir
+} from "./chunk-B5ZFTRRJ.js";
+import "./chunk-VQNPD2TN.js";
+import "./chunk-6GGGBUIJ.js";
+import "./chunk-AGCF4D2E.js";
+import "./chunk-W5G26WJ5.js";
+import "./chunk-HWYXSU2G.js";
+import "./chunk-JRFR6BLO.js";
+import "./chunk-MARUHEWW.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
