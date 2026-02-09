756528bf162d1a150ebab92d93125efa9f84a181
Arnauld-Mba <arnauldmba@gmail.com>
2026-02-09 20:34:57 +0100

    Fix gitignore and interceptor

--- frontend/src/app/services/token-interceptor.ts
+++ frontend/src/app/services/token-interceptor.ts
@@ -4,33 +4,6 @@ import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 
-/*
-
-export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
-
-  const excluded = ['/api/auth/login', '/api/auth/register', '/api/auth/verifyEmail'];
-  if (excluded.some(path => req.url.includes(path))) {
-    return next(req);
-  }
-
-  const token = authService.getToken();
-  const reqWithToken = token
-    ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
-    : req;
-
-  return next(reqWithToken).pipe(
-    catchError((err: HttpErrorResponse) => {
-      if (err.status === 401) {
-        authService.logout(); 
-        router.navigate(['/login'], { queryParams: { returnUrl: router.url } });
-      }
-      return throwError(() => err);
-    })
-  );
-};
-*/
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
@@ -64,11 +37,7 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
       if (isAuthProblem) {
         authService.logout();
 
-        // Option: stocker un message pour l’afficher sur la page login
-        // (selon ton app: toast/snackbar/service message)
         if (isDisabled) {
-          // ex: authService.setAuthMessage?.("Compte désactivé");
-          // ou localStorage/sessionStorage
           sessionStorage.setItem('auth_error', 'Compte désactivé. Contactez le support.');
         } else if (body?.message) {
           sessionStorage.setItem('auth_error', body.message);
@@ -81,4 +50,3 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
     })
   );
 };
-
